e685bebf6438b6a347d81114f62efa293834dd2e
Laura Darlak <laura@polyporedesign.com>
2018-09-09 14:57:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,10 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_name_input = gets.strip
-    artist = 
+    artist = Artist.find_by_name(artist_name_input)
+    artist.sort_by{|s| s.name}.each_with_index{|a, i|
+      puts "#{i+1}. #{s.name}"
+    }
   end
   
   
