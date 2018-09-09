2de2945b18c3992d8202cf4570f02eca9aabdaf1
Laura Darlak <laura@polyporedesign.com>
2018-09-09 15:07:14 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,6 +56,7 @@ class MusicLibraryController
     genre = Genre.find_by_name(genre_input)
     if genre != nil
       genre.songs.sort_by{|song| song.name}.each_with_index{|a, i|
+        binding.pry
         puts "#{i+1}. #{a.artist.name} - #{a.song}"
       }
     end
